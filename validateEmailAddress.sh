97354f3c571493f55735d925eba07083f756f517
unknown <ararky7@gmail.com>
2020-10-13 22:20:18 +0530

    validation of last part done

--- validateEmailAddress.sh
+++ validateEmailAddress.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter Email Id : " email;
 
-emailPattern="^[0-9a-zA-Z]+[._+-]+[0-9a-zA-Z]+@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$"
+emailPattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-z]{2,4}([.][a-zA-z]{2})?"
 
 if [[ $email =~ $emailPattern ]]
 then
