33974c77c358c78652ed5e0ab72d42205dff64d9
unknown <ararky7@gmail.com>
2020-10-13 22:00:42 +0530

    validation of optional part before @ and after .

--- validateEmailAddress.sh
+++ validateEmailAddress.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 
+# program to validate optional Part before @ and after .
+
 read -p "Enter Email Id : " email;
 
-emailPattern="^[0-9a-zA-Z]+[._+-]+[0-9a-zA-Z]+@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$"
+emailPattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$"
 
 if [[ $email =~ $emailPattern ]]
 then
