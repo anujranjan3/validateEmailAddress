c41afd7353d8c3d07dca115ca7f4528ad5561a6d
unknown <ararky7@gmail.com>
2020-10-13 21:35:47 +0530

    Validation of dot and Mandatory 3rd part is done

--- validateEmailAddress.sh
+++ validateEmailAddress.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter Email Id : " email;
 
-emailPattern="^[0-9a-zA-Z]+@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
+emailPattern="^[0-9a-zA-Z]+@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$"
 
 if [[ $email =~ $emailPattern ]]
 then
